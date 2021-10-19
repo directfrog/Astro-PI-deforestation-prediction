d95b99e6920db6932d14a1559968ce9c3869038c
directfrog <directfrog@gmail.com>
2021-10-19 12:09:19 +0100

    Update correlations.py

--- correlations.py
+++ correlations.py
@@ -5,24 +5,46 @@ from PIL import Image
 from scipy.stats import pearsonr
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
-VG = pd.read_csv('veg.csv').values.tolist()
-TMP = pd.read_csv('temp.csv').values.tolist()
 
+VG = pd.read_csv('temp-2000.csv').values.tolist()
+TMP = pd.read_csv('veg-2000.csv').values.tolist()
+
+
+'''
 vg = []
 for x in VG:
 	for i in x:
-		vg.append(i)
+		if i != 99999.0 and :
+			vg.append(i)
 
 tmp = []
 for x in TMP:
 	for i in x:
-		tmp.append(i)
+		if i != 99999.0:
+			tmp.append(i)
+'''
 
-if len(vg) > len(tmp):
-	vg = vg[0:len(tmp)]
-else:
-	tmp = tmp[0:len(vg)]
+vg = []
+tmp = []
+for row in range(len(TMP)):
+	for value in range(len(TMP[row])):
+		if TMP[row][value] != 99999.0 and VG[row][value] != 99999.0:
+			tmp.append(TMP[row][value])
+			vg.append(VG[row][value])
+
+
+total = 0
+for x in tmp:
+	if x == 99999.0:
+		total += 1 
+print(total)
+print(len(vg), len(tmp))
+#print(vg)
+
+#plt.plot(tmp, vg)
+#plt.show()
 
 corr = pearsonr(vg, tmp)
 print(corr)
